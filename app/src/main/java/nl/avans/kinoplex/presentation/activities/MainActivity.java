472a5a2eb23a118873828c8f8b4893ad3f30c587
LAPTOP-OPLBNVM3\roman <romano-keereweer@hotmail.com>
2019-03-29 14:08:57 +0100

    H3 search function

--- app/src/main/java/nl/avans/kinoplex/presentation/activities/MainActivity.java
+++ app/src/main/java/nl/avans/kinoplex/presentation/activities/MainActivity.java
@@ -1,8 +1,11 @@
 package nl.avans.kinoplex.presentation.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
 
 import java.util.ArrayList;
 
@@ -28,5 +31,15 @@ public class MainActivity extends AppCompatActivity {
 
     movieAdapter = new MovieAdapter(new ArrayList<>());
     DataMigration.getFactory().getMovieDao(550).readIntoAdapter(movieAdapter); // Async
+
+    Button button = findViewById(R.id.button);
+
+    button.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        Intent intent = new Intent(getApplicationContext(),SearchActivity.class);
+        startActivity(intent);
+      }
+    });
   }
 }
