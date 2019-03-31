e92ce28da3596189d10d81e073bd3ccab620c76c
Stijn Schep <stijnschep@outlook.com>
2019-03-31 12:41:23 +0200

    Finished layout for LoginActivity

--- app/src/main/java/nl/avans/kinoplex/presentation/activities/LoginActivity.java
+++ app/src/main/java/nl/avans/kinoplex/presentation/activities/LoginActivity.java
@@ -2,19 +2,26 @@ package nl.avans.kinoplex.presentation.activities;
 
 import android.app.Activity;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
 
 
 import nl.avans.kinoplex.R;
+import nl.avans.kinoplex.domain.Constants;
 
-import static java.security.AccessController.getContext;
 
-public class LoginActivity extends Activity {
+public class LoginActivity extends Activity implements View.OnClickListener {
+    private EditText usernameEditText;
+    private EditText passwordEditText;
+
+    private Button registerButton;
+    private Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +29,33 @@ public class LoginActivity extends Activity {
         setContentView(R.layout.activity_login);
 
         ImageView background = findViewById(R.id.iv_login_background);
-        Glide.with(this).load(R.drawable.login_background).into(background);
+        Glide.with(this).load(R.drawable.login_background_poster).into(background);
+
+        usernameEditText = findViewById(R.id.et_login_username);
+        passwordEditText = findViewById(R.id.et_login_password);
 
 
+        registerButton = findViewById(R.id.btn_login_register);
+        registerButton.setOnClickListener(this);
+
+        loginButton = findViewById(R.id.btn_login_login);
+        loginButton.setOnClickListener(this);
+    }
+
+
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.btn_login_login:
+                Log.d(Constants.LOGINACT_TAG, "User wants to log in...");
+
+                break;
+
+            case R.id.btn_login_register:
+                Log.d(Constants.LOGINACT_TAG, "User wants to register...");
+
+                break;
+        }
     }
 }
