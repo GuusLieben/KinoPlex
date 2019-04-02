74ad523eee0c80c0accb27c8c4cb7aa1480be5d7
Stijn Schep <stijnschep@outlook.com>
2019-04-02 16:24:07 +0200

    User can now see lists in ManageListActivity, add, edit and delete them. Changes are visible in runtime

--- app/src/main/java/nl/avans/kinoplex/business/CustomListChecker.java
+++ app/src/main/java/nl/avans/kinoplex/business/CustomListChecker.java
@@ -2,18 +2,19 @@ package nl.avans.kinoplex.business;
 
 import android.util.Log;
 
+
 public class CustomListChecker {
 
     public static boolean isCustomList(String listname) {
         Log.d(CustomListChecker.class.getCanonicalName(), "Checking if list " + listname + " is a custom list");
 
-        if (listname.equalsIgnoreCase("Now_playing")) {
+        if (listname.equalsIgnoreCase("!Now_playing")) {
             return false;
-        } else if (listname.equalsIgnoreCase("Popular")) {
+        } else if (listname.equalsIgnoreCase("!Popular")) {
             return false;
-        } else if (listname.equalsIgnoreCase("Top_rated")) {
+        } else if (listname.equalsIgnoreCase("!Top_rated")) {
             return false;
-        } else if (listname.equalsIgnoreCase("Upcoming")) {
+        } else if (listname.equalsIgnoreCase("!Upcoming")) {
             return false;
         } else {
             return true;
