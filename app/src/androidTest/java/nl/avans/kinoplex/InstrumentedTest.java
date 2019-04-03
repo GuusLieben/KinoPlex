5702eed02ed9c880a820f850f3bf0a4e4e94b31b
Guus Lieben <guus@xendox.com>
2019-04-03 21:03:54 +0200

    Add Json parse test to Instrumented Tests

--- app/src/androidTest/java/nl/avans/kinoplex/InstrumentedTest.java
+++ app/src/androidTest/java/nl/avans/kinoplex/InstrumentedTest.java
@@ -1,9 +1,13 @@
 package nl.avans.kinoplex;
 
+import android.net.Uri;
 import android.support.test.runner.AndroidJUnit4;
 
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -13,9 +17,11 @@ import java.util.HashMap;
 import java.util.Map;
 
 import nl.avans.kinoplex.business.FirestoreUtils;
+import nl.avans.kinoplex.business.JsonUtils;
 import nl.avans.kinoplex.domain.Constants;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 /**
@@ -53,6 +59,33 @@ public class InstrumentedTest {
         db.collection(Constants.COL_GENRES).document("-10").set(data)
                 .addOnSuccessListener(InstrumentedTest::onSuccess)
                 .addOnFailureListener(aVoid -> fail("Failed to write to Firestore"));
+    }
+
+    @Test
+    public void jsonIsParsedProperly() throws JSONException {
+        String sampleJsonUrl = "https://avans.studyhost.nl/JavaDoc%20Kinoplex/units/mock_json.json";
+        Uri sampleJsonUri = Uri.parse(sampleJsonUrl);
+        JSONObject result = JsonUtils.getJSONObjectFromUrl(sampleJsonUri);
+
+        JSONArray resultJSONArray = result.getJSONArray("results");
+        assertEquals(10, resultJSONArray.length());
+        if (resultJSONArray.length() != 10) fail();
+
+        for (int i = 0; i < 10; i++) {
+            JSONObject object = resultJSONArray.getJSONObject(i);
+            assertTrue(parseJsonObject(object));
+        }
+    }
+
+    private boolean parseJsonObject(JSONObject object) {
+        try {
+            object.getString("_id");
+            object.getInt("index");
+            object.getString("guid");
+            return true;
+        } catch (JSONException ex) {
+            return false;
+        }
     }
 
     @After
--- app/src/test/java/nl/avans/kinoplex/UnitTests.java
+++ app/src/test/java/nl/avans/kinoplex/UnitTests.java
@@ -1,8 +1,17 @@
 package nl.avans.kinoplex;
 
+import android.net.Uri;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import nl.avans.kinoplex.business.JsonUtils;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +19,7 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class UnitTests {
-    @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
-    }
+
+
+
 }
