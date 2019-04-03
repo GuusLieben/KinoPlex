d08200bafcf0a51e7db80173cc74b97cb6ee09c3
LAPTOP-OPLBNVM3\roman <romano-keereweer@hotmail.com>
2019-04-03 16:18:07 +0200

    Java docs update

--- app/src/main/java/nl/avans/kinoplex/presentation/viewholders/MovieViewHolder.java
+++ app/src/main/java/nl/avans/kinoplex/presentation/viewholders/MovieViewHolder.java
@@ -14,6 +14,7 @@ import nl.avans.kinoplex.presentation.activities.DetailActivity;
 
 import static nl.avans.kinoplex.domain.Constants.MAINMOVIEVH_TAG;
 
+
 public class MovieViewHolder extends AbstractViewHolder implements View.OnClickListener {
 
     private ImageView moviePoster;
@@ -21,6 +22,11 @@ public class MovieViewHolder extends AbstractViewHolder implements View.OnClickL
 
     private Movie movie;
 
+    /** @author Romano Keereweer
+     * Instantiates a new Movie view holder.
+     *
+     * @param itemView the item view
+     */
     public MovieViewHolder(@NonNull View itemView) {
         super(itemView);
         moviePoster = itemView.findViewById(R.id.image_view_movie_poster);
@@ -28,14 +34,29 @@ public class MovieViewHolder extends AbstractViewHolder implements View.OnClickL
         itemView.setOnClickListener(this);
     }
 
+    /** @author Romano Keereweer
+     * Gets movie poster.
+     *
+     * @return the movie poster
+     */
     public ImageView getMoviePoster() {
         return moviePoster;
     }
 
+    /** @author Romano Keereweer
+     * Gets movie title.
+     *
+     * @return the movie title
+     */
     public TextView getMovieTitle() {
         return movieTitle;
     }
 
+    /** @author Romano Keereweer
+     * Sets movie.
+     *
+     * @param movie the movie
+     */
     public void setMovie(Movie movie) {
         this.movie = movie;
     }
